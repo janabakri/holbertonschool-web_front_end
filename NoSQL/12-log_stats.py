ac43ff355654596e18989fc5e924a351353a87a4
jana <bkeyh382@gmail.com>
2026-04-27 17:47:01 +0300

    Remove log_stats.py script

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,15 +1,1 @@
-#!/usr/bin/env python3
-from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx = client.logs.nginx
-
-print("{} logs".format(nginx.count_documents({})))
-print("Methods:")
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = nginx.count_documents({"method": method})
-    print("\tmethod {}: {}".format(method, count))
-
-print("{} status check".format(
-    nginx.count_documents({"method": "GET", "path": "/status"})
-))
